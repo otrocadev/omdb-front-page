b24bbf9ef90b276af23ba9d498e594fa5267fd2a
Ot Roca <otrocadev@gmail.com>
2025-11-17 13:30:54 +0100

    feat: add Angular environment files to gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,6 @@ export const environment = {
   SUPABASE_URL: 'https://ztuiaiiyyqhujuofhcqq.supabase.co',
   SUPABASE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0dWlhaWl5eXFodWp1b2ZoY3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNjE5MjAsImV4cCI6MjA3NzkzNzkyMH0.WP0vB4ZungUSzGZvjhSqo-8uXEWVF8vNx4hFCOm9aMI',
+  OMDB_API_KEY: 'your_api_key',
+  OMDB_API_URL: 'https://www.omdbapi.com',
 };
---
+++ src/environments/environment_example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  SUPABASE_URL: 'your_supabase_database_api_url',
+  SUPABASE_KEY: 'your_supabase_database_api_key',
+  OMDB_API_KEY: 'your_api_key',
+  OMDB_API_URL: 'https://www.omdbapi.com',
+};
