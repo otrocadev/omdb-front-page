58e72349c1568737b35fe432f4e6de239e81c236
Ot Roca <otrocadev@gmail.com>
2025-11-18 13:03:09 +0100

    feat: improve movie search UI with conditional rendering and enhance year display styling

--- src/app/shared/data-access/omdb.service.ts
+++ src/app/shared/data-access/omdb.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import type { SearchResult } from '../../movies-search/types/movieResultsType';
 
 @Injectable({
   providedIn: 'root',
