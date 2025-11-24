6f6a2ce149cf3edde384b0b18cc29b3f67947ff3
Ot Roca <otrocadev@gmail.com>
2025-11-24 19:24:41 +0100

    ``` chore: configure environment variables with dotenv and build script ```

---
+++ mynode.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+const dotenv = require("dotenv").config({ path: "src/.env" });
+
+const envFile = `export const environment = {
+    production: false,
+    SUPABASE_URL: '${process.env.SUPABASE_URL}',
+    SUPABASE_KEY: '${process.env.SUPABASE_KEY}',
+    OMDB_API_KEY: '${process.env.OMDB_API_KEY}',
+    OMDB_API_URL: '${process.env.OMDB_API_URL}',
+};
+`;
+const targetPath = path.join(
+  __dirname,
+  "./src/environments/environment.development.ts"
+);
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.development.ts`
+    );
+  }
+});
